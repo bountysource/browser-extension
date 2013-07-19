d6ab387df98b12f86ee3863f37a99e99859a77cf
Matchu <matchu1993@gmail.com>
2013-07-18 23:52:08 -0700

    include backer data on issue page

--- issue.js
+++ issue.js
@@ -1,7 +1,14 @@
+function pluralize(count, noun) {
+  var phrase = count + " " + noun;
+  if (count !== 1) phrase += "s";
+  return phrase;
+}
+
 Bountysource.getIssueByURL(document.location.href, function(issue) {
     var commentWrapper = document.querySelector(".comment-body");
     var notice = document.createElement("a");
     notice.innerText = "Worth $" + parseFloat(issue.bounty_total).toFixed(2) +
+                       " from " + pluralize(issue.bounties.length, "backer") +
                        " on Bountysource!";
     notice.href = "https://www.bountysource.com/" + issue.frontend_path;
     commentWrapper.insertBefore(notice, commentWrapper.childNodes[0]);
