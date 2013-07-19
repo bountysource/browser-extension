8efb77ee0c5078ae4828173f02ea4a75bd44c87e
Matchu <matchu1993@gmail.com>
2013-07-19 14:20:54 -0700

    even prettier header

--- issue.js
+++ issue.js
@@ -4,31 +4,39 @@ function pluralize(count, noun) {
   return phrase;
 }
 
-function buildLink(issue, text, subpath) {
+function buildLink(issue, text, subpath, className) {
   var link = document.createElement("a");
   link.innerText = text;
   link.href = "https://www.bountysource.com/" + issue.frontend_path + subpath;
-  link.className = "minibutton";
+  link.className = "minibutton " + className;
   return link;
+}
+
+function buildHeader() {
+  var header = document.createElement("img");
+  header.src = chrome.runtime.getURL("/logo.png");
+  header.className = "bountysource-notice-header";
+  return header;
 }
 
 function buildClaimLink(issue) {
   var text = "Claim $" + parseFloat(issue.bounty_total).toFixed(2) +
              " bounty (" + pluralize(issue.bounties.length, "backer") + ")";
-  return buildLink(issue, text, "/solutions");
+  return buildLink(issue, text, "/solutions", "primary");
 }
 
 function buildIncreaseLink(issue) {
-  return buildLink(issue, "Increase bounty on Bountysource", "/bounties");
+  return buildLink(issue, "Increase bounty", "/bounties");
 }
 
 function buildCreateLink(issue) {
-  return buildLink(issue, "Create bounty on Bountysource", "");
+  return buildLink(issue, "Create bounty", "", "primary");
 }
 
 function buildNotice() {
   var notice = document.createElement("div");
   notice.className = "bountysource-notice discusion-topic-infobar";
+  notice.appendChild(buildHeader());
   return notice;
 }
 
