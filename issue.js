e8ea3dc482215ae97ecd188d55b942d7c52e3422
Matchu <matchu1993@gmail.com>
2013-07-19 00:17:22 -0700

    significantly prettier issue header

--- issue.js
+++ issue.js
@@ -4,12 +4,36 @@ function pluralize(count, noun) {
   return phrase;
 }
 
+function buildLink(issue, text, subpath) {
+  var link = document.createElement("a");
+  link.innerText = text;
+  link.href = "https://www.bountysource.com/" + issue.frontend_path + subpath;
+  link.className = "minibutton";
+  return link;
+}
+
+function buildClaimLink(issue) {
+  var text = "Claim $" + parseFloat(issue.bounty_total).toFixed(2) +
+             " bounty (" + pluralize(issue.bounties.length, "backer") + ")";
+  return buildLink(issue, text, "/solutions");
+}
+
+function buildIncreaseLink(issue) {
+  return buildLink(issue, "Increase bounty on Bountysource", "/bounties");
+}
+
+function buildNotice(issue) {
+  var notice = document.createElement("div");
+  notice.className = "bountysource-notice discusion-topic-infobar";
+  notice.appendChild(buildClaimLink(issue));
+  notice.appendChild(buildIncreaseLink(issue));
+  return notice;
+}
+
 Bountysource.getIssueByURL(document.location.href, function(issue) {
-    var commentWrapper = document.querySelector(".comment-body");
-    var notice = document.createElement("a");
-    notice.innerText = "Worth $" + parseFloat(issue.bounty_total).toFixed(2) +
-                       " from " + pluralize(issue.bounties.length, "backer") +
-                       " on Bountysource!";
-    notice.href = "https://www.bountysource.com/" + issue.frontend_path;
-    commentWrapper.insertBefore(notice, commentWrapper.childNodes[0]);
+    var topicWrapper = document.querySelector(".discussion-topic");
+    // Not a typo below; they use both discussion and discusion :/
+    var infobar = topicWrapper.querySelector(".discusion-topic-infobar");
+    var notice = buildNotice(issue);
+    topicWrapper.insertBefore(notice, infobar.nextSibling);
 });
