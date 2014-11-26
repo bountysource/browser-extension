64fae56cfea438974ed99cf0f3894b69d41c4fc0
Warren Konkel <wkonkel@gmail.com>
2014-11-25 17:51:40 -0800

    0.0.4 - add support for firefox

--- data/thumbs.js
+++ data/thumbs.js
@@ -1,36 +1,63 @@
-var ThumbBox = function(options) {
-  options = options || {};
-  this.issue_url = options.issue_url || document.location.href;
-  this.impression = options.impression;
-  if (options.size === 'small') {
-    this.container_class = 'bountysource-thumbs-box-mini';
-    this.image_path = chrome.extension.getURL('images/thumbsup-20.png');
-  } else {
-    this.container_class = 'bountysource-thumbs-box';
-    this.image_path = chrome.extension.getURL('images/thumbsup-32.png');
-  }
+(function() {
 
-  this.createDom();
-  this.setInfoBox('spinner');
-};
-
-ThumbBox.loadAllData = function(instances, attempts) {
-  var urls = [];
-  for (var i=0; i < instances.length; i++) {
-    urls.push(instances[i].issue_url);
-  }
-
-  attempts = (attempts || 0) + 1;
-  Bountysource.api({
-    method: 'POST',
-    path: '/thumbs/index',
-    body: {
-      urls: urls,
-      is_retry: (attempts > 1),
-      impression: instances[0].impression
+  var Bountysource = {
+    imagePath: function(image) {
+      if (window.chrome) {
+        return chrome.extension.getURL('data/images/' + image);
+      } else if (self && self.options && self.options.image_base) {
+        return self.options.image_base + image;
+      }
     },
-    success: function(response) {
-      if (response.length !== instances.length) {
+
+    api: function(request, callback) {
+      if (window.chrome) {
+        chrome.runtime.sendMessage({ action: "api_call", request: request }, callback);
+      } else if (self && self.port) {
+        var callback_str = "api_call_" + (new Date()).getTime();
+        self.port.once(callback_str, callback);
+        self.port.emit("api_call", { request: request, callback_str: callback_str });
+      }
+    }
+  };
+
+  var ThumbBox = function(options) {
+    options = options || {};
+    this.issue_url = options.issue_url || document.location.href;
+    this.impression = options.impression;
+    if (options.size === 'small') {
+      this.container_class = 'bountysource-thumbs-box-mini';
+      this.image_path = Bountysource.imagePath('thumbsup-20.png');
+    } else {
+      this.container_class = 'bountysource-thumbs-box';
+      this.image_path = Bountysource.imagePath('thumbsup-32.png');
+    }
+
+    this.createDom();
+    this.setInfoBox('spinner');
+  };
+
+  ThumbBox.loadAllData = function(instances, attempts) {
+    var urls = [];
+    for (var i=0; i < instances.length; i++) {
+      urls.push(instances[i].issue_url);
+    }
+
+    attempts = (attempts || 0) + 1;
+    Bountysource.api({
+      method: 'POST',
+      path: '/thumbs/index',
+      body: {
+        urls: urls,
+        is_retry: (attempts > 1),
+        impression: instances[0].impression
+      }
+    }, function(response) {
+      if (!response) {
+        console.log("ERROR3", response);
+        for (var i=0; i < instances.length; i++) {
+          instances[i].setInfoBox('ERR3');
+        }
+      } else if (response.length !== instances.length) {
         console.log("ERROR4, unexpected response", instances.length, response.length);
         for (var i=0; i < instances.length; i++) {
           instances[i].setInfoBox('ERR4');
@@ -61,67 +88,63 @@ ThumbBox.loadAllData = function(instances, attempts) {
           }
         }
       }
-    },
-    error: function(response) {
-      console.log("ERROR3", response);
-      for (var i=0; i < instances.length; i++) {
-        instances[i].setInfoBox('ERR3');
-      }
+    }.bind(this));
+  };
+
+  ThumbBox.prototype.createDom = function() {
+    this.container = document.createElement('div');
+    this.container.classList.add(this.container_class);
+
+    this.thumb_wrapper = document.createElement('div');
+    this.thumb_wrapper.classList.add('thumb-wrapper');
+    this.thumb_wrapper.addEventListener('click', this.thumbClicked.bind(this));
+    this.container.appendChild(this.thumb_wrapper);
+
+    this.thumb_image = document.createElement('img');
+    this.thumb_image.src = this.image_path;
+    this.thumb_wrapper.appendChild(this.thumb_image);
+
+    this.info_wrapper = document.createElement('div');
+    this.info_wrapper.classList.add('info-wrapper');
+    this.container.appendChild(this.info_wrapper);
+  };
+
+  ThumbBox.prototype.setInfoBox = function(text) {
+    if (text === 'spinner') {
+      var spinner_image = document.createElement('img');
+      spinner_image.src = Bountysource.imagePath('spinner.gif');
+      this.info_wrapper.innerHTML = '';
+      this.info_wrapper.appendChild(spinner_image);
+    } else {
+      this.info_wrapper.innerHTML = text;
     }
-  });
-};
+  };
 
-ThumbBox.prototype.createDom = function() {
-  this.container = document.createElement('div');
-  this.container.classList.add(this.container_class);
+  ThumbBox.prototype.setResponse = function(response) {
+    this.api_response = response;
+    this.setInfoBox(response.thumbs_up_count);
+    if (response.has_thumbed_up) {
+      this.container.classList.add('has-thumbed-up');
+    } else {
+      this.container.classList.remove('has-thumbed-up');
+    }
+  };
 
-  this.thumb_wrapper = document.createElement('div');
-  this.thumb_wrapper.classList.add('thumb-wrapper');
-  this.thumb_wrapper.addEventListener('click', this.thumbClicked.bind(this));
-  this.container.appendChild(this.thumb_wrapper);
-
-  this.thumb_image = document.createElement('img');
-  this.thumb_image.src = this.image_path;
-  this.thumb_wrapper.appendChild(this.thumb_image);
-
-  this.info_wrapper = document.createElement('div');
-  this.info_wrapper.classList.add('info-wrapper');
-  this.container.appendChild(this.info_wrapper);
-};
-
-ThumbBox.prototype.setInfoBox = function(text) {
-  if (text === 'spinner') {
-    var spinner_image = document.createElement('img');
-    spinner_image.src = chrome.extension.getURL('images/spinner.gif');
-    this.info_wrapper.innerHTML = '';
-    this.info_wrapper.appendChild(spinner_image);
-  } else {
-    this.info_wrapper.innerHTML = text;
-  }
-};
-
-ThumbBox.prototype.setResponse = function(response) {
-  this.api_response = response;
-  this.setInfoBox(response.thumbs_up_count);
-  if (response.has_thumbed_up) {
-    this.container.classList.add('has-thumbed-up');
-  } else {
-    this.container.classList.remove('has-thumbed-up');
-  }
-};
-
-ThumbBox.prototype.thumbClicked = function() {
-  if (this.api_response && this.api_response.issue_id) {
-    this.setInfoBox('spinner');
-    Bountysource.api({
-      method: 'POST',
-      path: '/thumbs',
-      body: {
-        issue_id: this.api_response.issue_id,
-        downvote: !!this.api_response.has_thumbed_up
-      },
-      success: function(response) {
-        if (response.redirect_to) {
+  ThumbBox.prototype.thumbClicked = function() {
+    if (this.api_response && this.api_response.issue_id) {
+      this.setInfoBox('spinner');
+      Bountysource.api({
+        method: 'POST',
+        path: '/thumbs',
+        body: {
+          issue_id: this.api_response.issue_id,
+          downvote: !!this.api_response.has_thumbed_up
+        },
+      }, function(response) {
+        if (!response) {
+          console.log("ERROR5", response);
+          this.setInfoBox('ERR5');
+        } else if (response.redirect_to) {
           document.location.href = response.redirect_to;
         } else if (response.issue_id) {
           this.setResponse(response);
@@ -129,21 +152,15 @@ ThumbBox.prototype.thumbClicked = function() {
           console.log("ERROR6", response);
           this.setInfoBox('ERR6');
         }
-      }.bind(this),
-      error: function(response) {
-        console.log("ERROR5", response);
-        this.setInfoBox('ERR5');
-      }.bind(this)
-    });
-  }
-};
+      }.bind(this));
+    }
+  };
 
 
-/* hook into the page DOM */
-(function() {
+
 
   // Bountysource
-  if (document.location.href.indexOf(Bountysource.www_base)===0) {
+  if (document.location.host.match(/bountysource\.com$/)===0) {
     document.body.classList.add('bountysource-thumbs-extension-is-installed');
 
   // Github (single page app so use fancy timers)
