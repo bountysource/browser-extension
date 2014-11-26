bfc1e974179b00a495fcc5e19b318cfea2137fd9
Warren Konkel <wkonkel@gmail.com>
2014-11-26 13:00:19 -0800

    bigger project icon. logged out state. status messages.

--- data/popup/home.js
+++ data/popup/home.js
@@ -10,34 +10,35 @@ angular.module('app').config(function($routeProvider) {
   $scope.img_src_logo = $bountysource.imagePath('Bountysource-green.png');
   $scope.img_src_thumbs20 = $bountysource.imagePath('thumbsup-20.png');
 
+  $scope.status = 'loading';
+
   $bountysource.api({
     path: 'people/me'
   }, function(response) {
-    $scope.current_person = response;
-    $scope.$digest();
-
     if (response.id) {
-      console.log("getting thumbs");
+      $scope.current_person = response;
+
       $bountysource.api({
         path: 'issues',
         params: {
           thumbed_by_person_id: response.id,
           order: 'thumbed_at',
-          include_team: true
+          include_team: true,
+          include_tracker: true
         }
       }, function(thumbed_response) {
-        $scope.thumbed_issues = thumbed_response;
+        if (thumbed_response.length > 0) {
+          $scope.thumbed_issues = thumbed_response;
+        } else {
+          $scope.status = 'no_issues';
+        }
         $scope.$digest();
       });
+    } else {
+      $scope.status = 'signin';
     }
+
+    $scope.$digest();
   });
-
-  // $bountysource.api({
-  //   path: 'teams'
-  // }, function(response) {
-  //   $scope.response = response;
-  // });
-
-
 
 });
